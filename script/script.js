7db4887115a3899c4202ab01425b67f2033ca757
PindelloDev <138374245+PindelloDevelopment@users.noreply.github.com>
2024-09-23 14:54:22 +0100

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+	$( ".scb" ).click(function() {
+	  $( ".scb" ).css('background', 'green');
+	  $( ".scb .card-body h5" ).css('color', 'white');
+	  $( ".scb .card-text" ).css('color', 'white');
+	});
+  });
+
+  $( document ).ready(function() {
+	$( ".srcb" ).click(function() {
+	  $( ".srcb" ).css('background', 'green');
+	  $( ".srcb .card-body h5" ).css('color', 'white');
+	  $( ".srcb .card-text" ).css('color', 'white');
+	});
+  });
+
+  $( document ).ready(function() {
+	$( ".cpq" ).click(function() {
+	  $( ".cpq" ).css('background', 'green');
+	  $( ".cpq .card-body h5" ).css('color', 'white');
+	  $( ".cpq .card-text" ).css('color', 'white');
+	});
+  });
